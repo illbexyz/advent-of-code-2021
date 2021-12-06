59e8b977b1376eef366acb686050c44db99a2867
Alberto Nicoletti <albyx.n@gmail.com>
2021-12-06 21:48:20 +0100

    (day_01): part 2

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# advent-of-code-2021
-Solutions for Advent of Code 2021 in Rust
+# Advent of Code 2021
+
+Solutions for Advent of Code 2021
+
+- [Day 01](https://github.com/illbexyz/advent-of-code-2021/tree/main/day-01/src/main.rs) (Rust)
--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -1,6 +1,6 @@
 use std::{fs, io};
 
-fn part_1(numbers: &[i32]) -> i32 {
+fn count_increments(numbers: &[i32]) -> i32 {
     let no_tail = &numbers[0..numbers.len() - 1];
     let no_head = &numbers[1..numbers.len()];
 
@@ -11,6 +11,23 @@ fn part_1(numbers: &[i32]) -> i32 {
         .sum()
 }
 
+fn part_1(numbers: &[i32]) -> i32 {
+    count_increments(&numbers)
+}
+
+fn part_2(numbers: &[i32]) -> i32 {
+    let no_head = &numbers[1..numbers.len()];
+    let no_head_2 = &numbers[2..numbers.len()];
+    let summed_triples: Vec<i32> = numbers
+        .iter()
+        .zip(no_head)
+        .zip(no_head_2)
+        .map(|((x, y), z)| x + y + z)
+        .collect();
+
+    count_increments(&summed_triples)
+}
+
 fn main() -> Result<(), io::Error> {
     let filename = "input.txt";
     let file_contents = fs::read_to_string(filename).expect("The file doesn't exist");
@@ -18,9 +35,13 @@ fn main() -> Result<(), io::Error> {
         .lines()
         .map(|line| line.parse().unwrap())
         .collect();
-    let increments = part_1(&numbers);
+    let p1 = part_1(&numbers);
 
-    println!("{}", increments);
+    println!("Part One: {}", p1);
+
+    let p2 = part_2(&numbers);
+
+    println!("Part Two: {}", p2);
 
     Ok(())
 }
