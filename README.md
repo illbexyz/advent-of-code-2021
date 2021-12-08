d849432d44e930d4e3be8a5bc891c082b3dc1fc9
Alberto Nicoletti <albyx.n@gmail.com>
2021-12-08 16:53:40 +0100

    (day-03): part two

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Solutions for Advent of Code 2021
 
 - [Day 01](https://github.com/illbexyz/advent-of-code-2021/tree/main/day-01/src/main.rs) (Rust)
 - [Day 02](https://github.com/illbexyz/advent-of-code-2021/tree/main/day-02/day-02.py) (Python)
+- [Day 03](https://github.com/illbexyz/advent-of-code-2021/tree/main/day-03/day_03.erl) (Erlang)
--- day-03/day_03.erl
+++ day-03/day_03.erl
@@ -13,52 +13,67 @@ readlines(FileName) ->
       after file:close(Device)
     end.
 
+invert_bit("1") -> "0";
+invert_bit("0") -> "1".
+
 get_column(Lines, ColIndex) -> 
   lists:map(
-    fun (Line) ->
-      string:slice(Line, ColIndex, 1)
-    end,
-  Lines).
+    fun (Line) -> string:slice(Line, ColIndex, 1) end,
+    Lines
+  ).
 
 bin_to_int(Binary) ->
-  LastIdx = length(Binary) - 1,
+  LastIdx = string:len(Binary) - 1,
   BinWithIndexes = lists:zip(lists:seq(0, LastIdx), Binary),
   Values = lists:map(
     fun ({ Idx, Bit }) ->
-      trunc(list_to_integer(Bit) * math:pow(2, (LastIdx - Idx)))
+      trunc(list_to_integer([Bit]) * math:pow(2, (LastIdx - Idx)))
     end,
     BinWithIndexes
   ),
   lists:sum(Values).
 
-gamma_bit(Column) ->
+most_common_bit(Column) ->
   Ones = lists:filter(fun (Num) -> Num == "1" end, Column),
-  case string:length(Ones) > string:length(Column) / 2 of
-    true -> "1";
-    false -> "0"
+  case string:length(Ones) >= (string:length(Column) / 2) of
+    false -> "0";
+    true -> "1"
   end.
+
+least_common_bit(Column) -> invert_bit(most_common_bit(Column)).
 
 gamma(Lines) ->
   ColIndexes = lists:seq(0, length(lists:nth(1, Lines)) - 1),
-  lists:map(fun (ColIdx) -> gamma_bit(get_column(Lines, ColIdx)) end, ColIndexes).
+  lists:flatmap(
+    fun (ColIdx) -> most_common_bit(get_column(Lines, ColIdx)) end,
+    ColIndexes
+  ).
 
 epsilon_from_gamma(Gamma) ->
-  lists:map(
-    fun (Bit) ->
-      case Bit of 
-        "0" -> "1";
-        "1" -> "0"
-      end
-    end,
-    Gamma
-  ).
+  lists:flatmap(fun (Bit) -> invert_bit([Bit]) end, Gamma).
+
+iter_p2([Line], _Fn, _ColIdx) -> Line;
+iter_p2(Lines, CommonBitFn, ColIdx) ->
+  CommonBit = CommonBitFn(get_column(Lines, ColIdx)),
+  RemainingLines = lists:filter(
+    fun (Line) -> string:slice(Line, ColIdx, 1) == CommonBit end,
+    Lines
+  ),
+  iter_p2(RemainingLines, CommonBitFn, ColIdx + 1).
 
 part_one(Lines) ->
   Gamma = gamma(Lines),
   Epsilon = epsilon_from_gamma(Gamma),
   bin_to_int(Gamma) * bin_to_int(Epsilon).
 
+part_two(Lines) ->
+  Oxygen = iter_p2(Lines, fun most_common_bit/1, 0),
+  CO2 = iter_p2(Lines, fun least_common_bit/1, 0),
+  bin_to_int(Oxygen) * bin_to_int(CO2).
+
 main() -> 
   Lines = string:split(readlines("input.txt"), "\n", all),
   PartOne = part_one(Lines),
-  io:format("Part One: ~p\n", [PartOne]).
+  io:format("Part One: ~p\n", [PartOne]),
+  PartTwo = part_two(Lines),
+  io:format("Part Two: ~p\n", [PartTwo]).
